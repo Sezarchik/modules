f4cb0369eee7799c0af089b2de65656498116cf3
Sezarchik <99130273+Sezarchik@users.noreply.github.com>
2023-11-28 17:18:10 +0300

    Update Saver.py

--- Saver.py
+++ Saver.py
@@ -35,18 +35,12 @@ class SaverMod(loader.Module):
     async def бляcmd(self, m: types.Message):
         ".бля <reply> - скачать самоуничтожающееся фото"
         reply = await m.get_reply_message()
-        try:
-            if not reply or not reply.media or not reply.media.ttl_seconds:
-                return await m.edit("бля")
-            await m.delete()
-            new = io.BytesIO(await reply.download_media(bytes))
-            new.name = reply.file.name
-            await m.client.send_file("me", new)
-        except AttributeError:
-            await m.delete()
-            new = io.BytesIO(await reply.download_media(bytes))
-            new.name = reply.file.name
-            await m.client.send_file("me", new)
+        if not reply or not reply.media or not reply.media.ttl_seconds:
+            return await m.edit("бля")
+        await m.delete()
+        new = io.BytesIO(await reply.download_media(bytes))
+        new.name = reply.file.name
+        await m.client.send_file("me", new)
 
     @loader.unrestricted
     async def swбляcmd(self, m: types.Message):
